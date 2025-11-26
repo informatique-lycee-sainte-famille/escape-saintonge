34a91b61a7af67a8d9e8ee2382491fce426f014e
Julien Flusin <julienflusin3@gmail.com>
2025-11-26 02:25:07 +0100

    Corrige le chemin de BASE_DIR et met à jour les définitions des répertoires statiques et médias dans settings.py

--- EscapeSaintonge/settings.py
+++ EscapeSaintonge/settings.py
@@ -14,11 +14,18 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = "/app"
-STATIC_DIR = BASE_DIR / 'static'
-MEDIA_DIR = BASE_DIR / 'media'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+BASE_DIR = "/app/"
+STATIC_DIR = BASE_DIR + 'static/'
+MEDIA_DIR = BASE_DIR + 'media/'
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -123,14 +130,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
